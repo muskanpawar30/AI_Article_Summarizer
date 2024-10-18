9fa08cb6d4a8ac4eec2cd639d1782b53389b21d9
muskanpawar30 <muskanpawar030106@gmail.com>
2024-10-18 11:56:17 +0530

    Implemented summary saving functionality and updated UI

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
-
-//import '../App.css'
 
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
-  const [btn,setButton] = useState(false)
+  const [showSummary, setShowSummary] = useState(false);
   const [allArticles, setAllArticles] = useState([]);
   const [copied, setCopied] = useState("");
+  const summaryRef = useRef(null);
 
   // RTK lazy query
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
@@ -21,105 +20,99 @@ const Demo = () => {
     const articlesFromLocalStorage = JSON.parse(
       localStorage.getItem("articles")
     );
-
     if (articlesFromLocalStorage) {
       setAllArticles(articlesFromLocalStorage);
     }
+
+    // Event listener to handle click outside
+    const handleClickOutside = (event) => {
+      if (summaryRef.current && !summaryRef.current.contains(event.target)) {
+        setShowSummary(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, []);
 
-  const handleClick = (item)=>{
-
-    setButton(true);
-    setArticle(item);
-  }
-
-              
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setButton(true);
 
     const existingArticle = allArticles.find(
       (item) => item.url === article.url
     );
 
-    if (existingArticle)
-      return setArticle(existingArticle);
-    
+    if (existingArticle) {
+      setArticle(existingArticle);
+      setShowSummary(true);
+      return;
+    }
 
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
       const updatedAllArticles = [newArticle, ...allArticles];
 
-      // update state and local storage
+      // Update state and local storage
       setArticle(newArticle);
-     
       setAllArticles(updatedAllArticles);
       localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-    }
-  };
-
-  // copy the url and toggle the icon for user feedback
-  const handleCopy = (copyUrl) => {
-    setCopied(copyUrl);
-    navigator.clipboard.writeText(copyUrl);
-    setTimeout(() => setCopied(false), 3000);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.keyCode === 13) {
-      handleSubmit(e);
+      setShowSummary(true);
     }
   };
 
   return (
-    <section className={btn?'flex flex-row mt-16 gap-8  w-full max-w-xl':'flex flex-col mt-16 gap-8 w-full m-auto max-w-xl'}>
-
-      {/* Search */}
+    <section
+      className={`search-summary-container ${
+        showSummary ? "show-summary" : ""
+      }`}
+    >
+      {/* Search Section */}
       <div className="flex flex-col w-full gap-2">
         <form
-          className='relative flex justify-center items-center'
+          className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
-            alt='link-icon'
-            className='absolute left-0 my-2 ml-3 w-5'
+            alt="link-icon"
+            className="absolute left-0 my-2 ml-3 w-5"
           />
-
           <input
-            type='url'
-            placeholder='Paste the article link'
+            type="url"
+            placeholder="Paste the article link"
             value={article.url}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            onKeyDown={handleKeyDown}
             required
-            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+            className="url_input peer"
           />
           <button
-            type='submit'
-            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
           >
-            <p>↵</p>
+            ↵
           </button>
         </form>
 
         {/* Browse History */}
-        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
-          {allArticles.reverse().map((item, index) => (
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
             <div
               key={`link-${index}`}
-              onClick={() => handleClick(item)}
-              className='link_card'
+              onClick={() => setArticle(item)}
+              className="link_card"
             >
-              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <img
                   src={copied === item.url ? tick : copy}
                   alt={copied === item.url ? "tick_icon" : "copy_icon"}
-                  className='w-[40%] h-[40%] object-contain'
+                  className="w-[40%] h-[40%] object-contain"
                 />
               </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
                 {item.url}
               </p>
             </div>
@@ -127,40 +120,38 @@ const Demo = () => {
         </div>
       </div>
 
-      {/* Display Result */}
-      <div className="flex flex-col w-full gap-2">
-      <div className='my-0 flex justify-center items-center'>
-        {isFetching ? (
-          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
-        ) : error ? (
-          <p className='font-inter font-bold text-black text-center'>
-            Well, that wasn't supposed to happen...
-            <br />
-            
-            <span className='font-satoshi font-normal text-gray-700'>
-              {error?.data?.error}
-            </span>
-            
-          </p>
-        ) : (
-          article.summary && (
-            <div className=' flex flex-col gap-3'>
-              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
-                Article <span className='blue_gradient'>Summary</span>
-              </h2>
-              <div className='summary_box max-w-full'>
-                <p className=' font-inter customW max-w-xl  font-medium text-sm text-gray-700'>
+      {/* Summary Section */}
+      {showSummary && (
+        <div
+          ref={summaryRef}
+          className={`summary_box ${showSummary ? "visible" : "hidden"}`}
+        >
+          {isFetching ? (
+            <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+          ) : error ? (
+            <p className="font-inter font-bold text-black text-center">
+              Well, that wasn't supposed to happen...
+              <br />
+              <span className="font-satoshi font-normal text-gray-700">
+                {error?.data?.error}
+              </span>
+            </p>
+          ) : (
+            article.summary && (
+              <div className="flex flex-col gap-3">
+                <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                  Article <span className="blue_gradient">Summary</span>
+                </h2>
+                <p className="font-inter font-medium text-sm text-gray-700">
                   {article.summary}
                 </p>
               </div>
-            </div>
-          )
-        )}
-      </div>
-    </div>
+            )
+          )}
+        </div>
+      )}
     </section>
   );
 };
 
 export default Demo;
-
